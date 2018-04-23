dc0fac3ff4ada84a4d7c3322324d5649c5160501
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-23 00:14:20 -0400

    commented out functions to activate modal

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -14,73 +14,73 @@
     
 // })
 
-$("#noMessage").hide();
+// $("#noMessage").hide();
 
-//function to show the modal
-function showModal(data) {
-    $("#modalNotes").empty();
+// //function to show the modal
+// function showModal(data) {
+//     $("#modalNotes").empty();
 
-    if (data.note) {
-        $("#modalNotes").append("<div class='card mod'>" +
-            "<div class='card-block notesDiv'>" +
-            "<form action='/note/" + data.note._id + "' method='POST'>" +
-            "<button class='btn btn-primary float-right del' type='submit'>" +
-            "Remove" +
-            "</button>" +
-            "</form>" +
-            data.note.body +
-            "</div>" +
-            "<div id='articleinfo' style='display:none'>" + data._id + "</div>" +
-            "</div>");
+//     if (data.note) {
+//         $("#modalNotes").append("<div class='card mod'>" +
+//             "<div class='card-block notesDiv'>" +
+//             "<form action='/note/" + data.note._id + "' method='POST'>" +
+//             "<button class='btn btn-primary float-right del' type='submit'>" +
+//             "Remove" +
+//             "</button>" +
+//             "</form>" +
+//             data.note.body +
+//             "</div>" +
+//             "<div id='articleinfo' style='display:none'>" + data._id + "</div>" +
+//             "</div>");
 
-        $('#articlenoteModal').modal('show');
-    } else {
+//         $('#articlenoteModal').modal('show');
+//     } else {
 
-        $("#modalNotes").append("<div class='card mod'>" +
-            "<div class='card-block'>" +
-            "No notes for this article yet!" +
-            "</div>" +
-            "<div id='articleinfo' style='display:none'>" + data._id + "</div>" +
-            "</div>");
-        $('#articlenoteModal').modal('show');
-    }
-}
+//         $("#modalNotes").append("<div class='card mod'>" +
+//             "<div class='card-block'>" +
+//             "No notes for this article yet!" +
+//             "</div>" +
+//             "<div id='articleinfo' style='display:none'>" + data._id + "</div>" +
+//             "</div>");
+//         $('#articlenoteModal').modal('show');
+//     }
+// }
 
-//click handler to add a note
-$(".addNote").on("click", function () {
-    //get the article database _id
-    var artID = $(this).attr("data-artid");
-    //make a GET request to /article/:id
-    $.ajax({
-        url: "/article/" + artID,
-        method: "GET"
-    }).done(function (data) {
-        showModal(data);
-    });
-});
+// //click handler to add a note
+// $(".addNote").on("click", function () {
+//     //get the article database _id
+//     var artID = $(this).attr("data-artid");
+//     //make a GET request to /article/:id
+//     $.ajax({
+//         url: "/article/" + artID,
+//         method: "GET"
+//     }).done(function (data) {
+//         showModal(data);
+//     });
+// });
 
-//click handler to save a note
-$("#saveNote").on("click", function () {
-    //get the note text and the article database _id
-    var newnote = $("#textarea").val().trim();
-    var artID = $("#articleinfo").text();
+// //click handler to save a note
+// $("#saveNote").on("click", function () {
+//     //get the note text and the article database _id
+//     var newnote = $("#textarea").val().trim();
+//     var artID = $("#articleinfo").text();
 
-    if (newnote !== "") {
-        //make a POST requestto /article/:id
-        $.ajax({
-            url: "/article/" + artID,
-            method: "POST",
-            data: {
-                body: newnote
-            }
-        }).done(function () {
-            location.assign("/saved");
-        });
-    } else {
-        $("#noMessage").show();
-    }
-});
+//     if (newnote !== "") {
+//         //make a POST requestto /article/:id
+//         $.ajax({
+//             url: "/article/" + artID,
+//             method: "POST",
+//             data: {
+//                 body: newnote
+//             }
+//         }).done(function () {
+//             location.assign("/saved");
+//         });
+//     } else {
+//         $("#noMessage").show();
+//     }
+// });
 
-$("#noArticlesHomeLink").on("click", function () {
-    location.assign("/");
-});
+// $("#noArticlesHomeLink").on("click", function () {
+//     location.assign("/");
+// });
